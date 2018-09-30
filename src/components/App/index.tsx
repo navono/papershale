1f82b120ce3ab68a8408c4d1294ced9e94984e4f
navono <navono007@gmail.com>
2018-09-30 15:56:49 +0800

    feat: add some tool test code

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+// import { PaperScope, Path, Color, Point, Tool, ToolEvent } from 'paper';
 import { PaperScope } from 'paper';
 import './index.scss';
 import * as actions from './actions';
@@ -28,6 +29,21 @@ class App extends React.Component<IProps, any> {
   public componentDidMount() {
     this.paper.setup(this.canvasRef.current as HTMLCanvasElement);
     this.props.actionDispatcher.updatePaper(this.paper);
+
+    // const circle = new Path.Circle(new Point([150, 150]), 50);
+    // const color = new Color(255, 0, 0);
+    // circle.fillColor = color;
+    // circle.selected = true;
+
+    // console.log('circle seg ', circle.segments);
+    // const seg = circle.segments[2];
+    // seg.selected = true;
+
+    // const tool = new Tool();
+    // tool.onMouseDown = (e: ToolEvent) => {
+    //   console.log('mouse down ', e);
+    //   seg.point = seg.point.add(e.delta);
+    // };
   }
 
   public render() {
