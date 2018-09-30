8a5c9e914df8890070a665b3159547cfb8c13b1a
navono <navono007@gmail.com>
2018-09-30 13:28:26 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import './App.scss';
+import { PaperScope, Path, Point, Color } from 'paper';
+
+class App extends React.Component {
+  private canvasRef: React.RefObject<HTMLCanvasElement>;
+  private paper: PaperScope;
+
+  constructor(props: any) {
+    super(props);
+    
+    this.canvasRef = React.createRef();
+    this.paper = new PaperScope();
+  }
+
+  public componentDidMount() {
+    this.paper.setup(this.canvasRef.current as HTMLCanvasElement);
+
+    const circle = new Path.Circle(new Point([50, 50]), 50);
+    const color = new Color(255, 0, 0);
+    circle.fillColor = color;
+    console.log(circle);
+  }
+
+  public render() {
+    return (
+      <div className="App">
+        <canvas id="canvas" ref={this.canvasRef} width={800} height={500}/> 
+      </div>
+    );
+  }
+}
+
+export default App;
